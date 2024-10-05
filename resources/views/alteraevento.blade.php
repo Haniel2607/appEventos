915b41245d7d6bfeaa6279d61123d96d8bdf7a69
Haniel2607 <hanielschramm@gmail>
2024-10-04 22:36:52 -0300

    cheio de erro

--- resources/views/alteraevento.blade.php
+++ resources/views/alteraevento.blade.php
@@ -1,6 +1,36 @@
 @extends('layout')
 @section('content')
 
-<h1>Está é a página de alterar os eventos</h1>
+@extends('layout')
+@section('content')
+<div class="container mt-5">
+    <h2>Alterar registros do Evento</h2>
+    <form method="post" action="{{route('altera-evento', $registrosEvento->idEvento)}}">
+        @csrf
+        @method('put')
+
+        <div class="col-md-6">
+            <label for="inputNomeEvento" class="form-label">Nome Evento</label>
+            <input type="text" class="form-control" id="nomeEvento" name="nomeEvento"
+                value="{{$registrosEvento->nomeEvento}}" placeholder="Digite o nome do Evento" required>
+        </div>
+        <div class="col-md-6">
+            <label for="inputDataEvento" class="form-label">Data Evento</label>
+            <input type="date" class="form-control" id="dataEvento" name="dataEvento"
+                value="{{$registrosEvento->dataEvento}}" placeholder="dd/mm/aaaa" required>
+        </div>
+        <div class="col-12">
+            <label for="inputAddress" class="form-label">Local Evento</label>
+            <input type="text" class="form-control" id="localEvento" name="localEvento"
+                value="{{$registrosEvento->localEvento}}" placeholder="Digite o Local do Evento" required>
+        </div>
+        <div class="col-12">
+            <label for="inputAddress2" class="form-label">Imagem do Evento (URL)</label>
+            <input type="text" class="form-control" id="imgEvento" name="imgEvento"
+                value="{{$registrosEvento->imgEvento}}" placeholder="Digite a URL da imagem" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Cadastrar</button>
+    </form>
+</div>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,15 @@ use App\Http\Controllers\EventosController;
 Route::get('/', [EventosController::class, 'MostrarHome'])->name('home-adm');
 Route::get('/cadastro-evento', [EventosController::class, 'MostrarCadastroEvento'])->name('show-cadastra-evento');
 Route::get('/lista-evento', [EventosController::class, 'MostrarEventoNome'])->name('lista-evento');
-Route::get('/altera-evento', [EventosController::class, 'MostrarEventoCodigo'])->name('show-altera-evento');
+Route::get('/altera-evento/{id}', [EventosController::class, 'MostrarEventoCodigo'])->name('show-altera-evento');
 
 //cadastrar
 Route::post('/cadastro-evento', [EventosController::class, 'CadastrarEventos'])->name('cadastra-evento');
 
 //deletar
-Route::delete('/apaga-evento', [EventosController::class, 'Destroy'])->name('apaga-evento');
+Route::delete('/apaga-evento/{id}', [EventosController::class, 'Destroy'])->name('apaga-evento');
 
 //alterar
-Route::put('/altera-evento', [EventosController::class, 'Update'])->name('altera-evento');
+Route::put('/altera-evento/{id}', [EventosController::class, 'Update'])->name('altera-evento');
 
 
